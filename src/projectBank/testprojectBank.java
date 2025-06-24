1dc33b4581f070ebf28a74e992bccc98ddd5142c
AdilAli02 <cavtappdevirb07@gmail.com>
2025-06-24 11:18:04 +0300

    Add files via upload

---
+++ src/projectBank/testprojectBank.java
@@ -0,0 +1,45 @@
+package projectBank;
+
+import java.util.Scanner;
+
+public class testprojectBank {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		Account account = new Account();
+
+		
+		System.out.print("Enter initial balance: ");
+		double initialBalance = input.nextDouble();
+		input.nextLine(); 
+		
+		
+		System.out.print("Set your password: ");
+		String pass = input.nextLine();
+		account.createAccount(initialBalance, pass);
+
+		
+		System.out.print("Enter password to view balance: ");
+		String enteredPass = input.nextLine();
+		account.getBalance(enteredPass);
+
+		
+		System.out.print("Enter amount to deposit: ");
+		double depositAmount = input.nextDouble();
+		input.nextLine();
+		
+		System.out.print("Enter password: ");
+		String depositPass = input.nextLine();
+		account.deposit(depositAmount, depositPass);
+
+		
+		System.out.print("Enter amount to withdraw: ");
+		double withdrawAmount = input.nextDouble();
+		input.nextLine();
+		
+		System.out.print("Enter password: ");
+		String withdrawPass = input.nextLine();
+		account.withdraw(withdrawAmount, withdrawPass);
+	}
+}
